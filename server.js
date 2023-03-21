bfc63f7bf9e5ea5a7816156b9836219aca1e0e3e
axemonkey <hi+github@clivemurray.com>
2023-03-21 15:54:51 +0000

    tidy up server.js

--- server.js
+++ server.js
@@ -1,17 +1,11 @@
-// import express from 'express';
-// import vhost from 'vhost';
-
 import {loveandpainkillersApp} from './loveandpainkillers.com/app.js';
 
-// const app = express();
 const env = process.env.NODE_ENV || 'development';
 
 if (env === 'development') {
 	loveandpainkillersApp
-		// .use(vhost('loveandpainkillers.node', loveandpainkillersApp))
 		.listen(7777);
 } else if (env === 'production') {
 	loveandpainkillersApp
-		// .use(vhost('loveandpainkillers.com', loveandpainkillersApp))
-		.listen(process.env.PORT); // heroku specifies port number
+		.listen(process.env.PORT); // host specifies port number
 }
